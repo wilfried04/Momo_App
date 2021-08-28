cfcf367ed900a087be1a51afc2f6400e8be9daab
wilfried04 <wilfriedkp.romeo@gmail.com>
2021-08-28 10:58:19 +0100

    config root home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  devise_for :users #permetr de gere toutes les routes qui on rapport a la connexion et pour les Users
+  root 'home#index'
   get 'home/private'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
